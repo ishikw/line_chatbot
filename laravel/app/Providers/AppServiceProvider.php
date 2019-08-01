4379d5099b5ef14ea680320aaf9e9f944f3d8033
metyarin <hideki.n@x-point-1.net>
2019-08-02 01:25:59 +0900

    adminatorのインストール

--- laravel/app/Providers/AppServiceProvider.php
+++ laravel/app/Providers/AppServiceProvider.php
@@ -2,26 +2,31 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
-
     /**
      * Bootstrap any application services.
      *
      * @return void
      */
     public function boot()
+    {
+        if (!defined('ADMIN')) {
+           define('ADMIN', config('variables.APP_ADMIN', 'admin'));
+        }
+        require_once base_path('resources/macros/form.php');
+        Schema::defaultStringLength(191);
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
     {
         //
     }
